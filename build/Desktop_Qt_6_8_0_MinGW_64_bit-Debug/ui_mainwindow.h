fe442d40f685a5f9d9cef6592e19b9a649a7215a
Juanvalencia0009 <juan.valencia11@utp.edu.co>
2024-10-23 21:29:22 -0500

    juan
    posible organizacion final de los botones

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -13,13 +13,16 @@
 #include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QListView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSlider>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTreeView>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -43,6 +46,8 @@ public:
     QPushButton *pushButton_VolumeV;
     QSlider *horizontalSlider_VolumeV;
     QLabel *label;
+    QTreeView *exploradorcarpetas;
+    QListView *listView;
     QMenuBar *menubar;
     QMenu *menuOpen;
     QMenu *menuVideo;
@@ -79,7 +84,7 @@ public:
         label_Value_File_Fame->setFont(font);
         horizontalSlide_DurationV = new QSlider(centralwidget);
         horizontalSlide_DurationV->setObjectName("horizontalSlide_DurationV");
-        horizontalSlide_DurationV->setGeometry(QRect(290, 380, 581, 20));
+        horizontalSlide_DurationV->setGeometry(QRect(290, 380, 571, 20));
         horizontalSlide_DurationV->setOrientation(Qt::Orientation::Horizontal);
         label_Current_Time = new QLabel(centralwidget);
         label_Current_Time->setObjectName("label_Current_Time");
@@ -90,34 +95,40 @@ public:
         label_Current_Time->setFont(font1);
         label_Total_Time = new QLabel(centralwidget);
         label_Total_Time->setObjectName("label_Total_Time");
-        label_Total_Time->setGeometry(QRect(810, 400, 71, 20));
+        label_Total_Time->setGeometry(QRect(800, 400, 71, 20));
         label_Total_Time->setFont(font1);
         pushButton_Play_PauseV = new QPushButton(centralwidget);
         pushButton_Play_PauseV->setObjectName("pushButton_Play_PauseV");
-        pushButton_Play_PauseV->setGeometry(QRect(620, 430, 51, 41));
+        pushButton_Play_PauseV->setGeometry(QRect(550, 430, 51, 41));
         pushButton_Seek_BackwardV = new QPushButton(centralwidget);
         pushButton_Seek_BackwardV->setObjectName("pushButton_Seek_BackwardV");
-        pushButton_Seek_BackwardV->setGeometry(QRect(500, 430, 51, 41));
+        pushButton_Seek_BackwardV->setGeometry(QRect(430, 430, 51, 41));
         pushButton_StopV = new QPushButton(centralwidget);
         pushButton_StopV->setObjectName("pushButton_StopV");
-        pushButton_StopV->setGeometry(QRect(380, 430, 51, 41));
+        pushButton_StopV->setGeometry(QRect(790, 430, 51, 41));
         pushButton_Seek_ForwardV = new QPushButton(centralwidget);
         pushButton_Seek_ForwardV->setObjectName("pushButton_Seek_ForwardV");
-        pushButton_Seek_ForwardV->setGeometry(QRect(730, 430, 51, 41));
+        pushButton_Seek_ForwardV->setGeometry(QRect(670, 430, 51, 41));
         pushButton_VolumeV = new QPushButton(centralwidget);
         pushButton_VolumeV->setObjectName("pushButton_VolumeV");
-        pushButton_VolumeV->setGeometry(QRect(100, 430, 51, 41));
+        pushButton_VolumeV->setGeometry(QRect(310, 430, 51, 41));
         horizontalSlider_VolumeV = new QSlider(centralwidget);
         horizontalSlider_VolumeV->setObjectName("horizontalSlider_VolumeV");
-        horizontalSlider_VolumeV->setGeometry(QRect(40, 380, 181, 20));
+        horizontalSlider_VolumeV->setGeometry(QRect(50, 440, 181, 20));
         horizontalSlider_VolumeV->setOrientation(Qt::Orientation::Horizontal);
         label = new QLabel(centralwidget);
         label->setObjectName("label");
-        label->setGeometry(QRect(80, 160, 37, 12));
+        label->setGeometry(QRect(80, 0, 37, 12));
+        exploradorcarpetas = new QTreeView(centralwidget);
+        exploradorcarpetas->setObjectName("exploradorcarpetas");
+        exploradorcarpetas->setGeometry(QRect(30, 30, 221, 91));
+        listView = new QListView(centralwidget);
+        listView->setObjectName("listView");
+        listView->setGeometry(QRect(30, 131, 221, 281));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 892, 18));
+        menubar->setGeometry(QRect(0, 0, 892, 22));
         menuOpen = new QMenu(menubar);
         menuOpen->setObjectName("menuOpen");
         menuVideo = new QMenu(menubar);
